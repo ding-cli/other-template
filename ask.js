cfc23d9c28dcb6938e0de319012c68fa8193101d
dingding <1060340716@qq.com>
2019-12-24 11:25:42 +0800

    修改 ask 文件

--- ask.js
+++ ask.js
@@ -1,3 +1,22 @@
-ask = {
-    aaa: '111'
-}
+module.exports = [
+    {
+        type: 'confirm',
+        name: 'private',
+        message: 'ths resgistery is private?',
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'author?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'description?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'license?',
+    },
+]
